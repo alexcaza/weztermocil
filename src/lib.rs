65a74eb0a5a2ad11b3001f6e1c6e28804b53f224
Alex Caza <alex@alexcaza.com>
2023-08-30 20:07:47 -0400

    Move strip_trailing_newline

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,4 @@
 static WEZTERM_CLI: &str = "wezterm";
 
+pub mod format;
 pub mod wezterm;
-
-pub fn strip_trailing_newline(input: &str) -> &str {
-    input
-        .strip_suffix("\r\n")
-        .or(input.strip_suffix("\n"))
-        .unwrap_or(input)
-}
