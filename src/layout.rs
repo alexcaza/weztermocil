12c407e3daf19df56a4cb35922f71a1f4d657b25
Alex Caza <alex@alexcaza.com>
2023-09-05 16:24:33 -0400

    Start implementing layouts

--- src/layout.rs
+++ src/layout.rs
@@ -1,23 +1,48 @@
+use std::fmt;
+
+#[derive(PartialEq)]
+pub struct NumPanes(i32);
+
+impl fmt::Display for NumPanes {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "{}", self)
+    }
+}
+
 pub enum Layout {
-    Even,
-    Odd,
+    EvenHorizontal,
+    EvenVertical,
+    MainVertical,
+    MainVerticalFlipped,
+    Tiled,
+    ThreeColumns,
+    DoubleMainHorizontal,
+    DoubleMainVertical,
 }
 
 impl Layout {
-    pub fn create(&self, num_panes: i32) {
+    pub fn create(&self, num_panes: NumPanes) {
+        if num_panes == NumPanes(1) {
+            single_pane()
+        }
+
         match self {
-            Layout::Even => even_layout(num_panes),
-            Layout::Odd => odd_layout(num_panes),
+            Layout::EvenHorizontal => even_layout(num_panes),
+            Layout::EvenVertical => even_layout(num_panes),
+            Layout::MainVertical => even_layout(num_panes),
+            Layout::MainVerticalFlipped => even_layout(num_panes),
+            Layout::Tiled => even_layout(num_panes),
+            Layout::ThreeColumns => even_layout(num_panes),
+            Layout::DoubleMainVertical => even_layout(num_panes),
+            Layout::DoubleMainHorizontal => even_layout(num_panes),
         }
     }
 }
 
-fn even_layout(num_panes: i32) {
-    // TODO do stuff
-    println!("even_layout::num_panes: {}", num_panes);
+fn single_pane() {
+    // TODO: Create pane
 }
 
-fn odd_layout(num_panes: i32) {
-    // todo do stuff
-    println!("odd_layout::num_panes: {}", num_panes);
+fn even_layout(num_panes: NumPanes) {
+    println!("even_layout::num_panes: {}", num_panes);
 }
