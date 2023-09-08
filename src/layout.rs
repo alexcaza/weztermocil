9ffccf8963dbe2242a96706fcb527610941d6458
Alex Caza <alex@alexcaza.com>
2023-09-08 18:46:46 -0400

    Rename NumPanes -> TotalPanes
    Much clearer API. Was actually the total number of panes, not the number of panes needed.

--- src/layout.rs
+++ src/layout.rs
@@ -3,9 +3,9 @@ use std::fmt;
 use crate::wezterm::pane::{Pane, SplitDirection};
 
 #[derive(PartialEq, Clone, Copy)]
-pub struct NumPanes(pub usize);
+pub struct TotalPanes(pub usize);
 
-impl fmt::Display for NumPanes {
+impl fmt::Display for TotalPanes {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         write!(f, "{}", self)
     }
@@ -23,8 +23,8 @@ pub enum Layout {
 }
 
 impl Layout {
-    pub fn create(&self, num_panes: NumPanes, parent_pane: Pane) -> Option<Vec<Pane>> {
-        if num_panes == NumPanes(1) {
+    pub fn create(&self, total_panes: TotalPanes, parent_pane: Pane) -> Option<Vec<Pane>> {
+        if total_panes == TotalPanes(1) {
             // Skip doing any pane creation
             // if there's only 1 pane being passed.
             // We can just run the command in the
@@ -33,12 +33,12 @@ impl Layout {
         }
 
         match self {
-            Layout::EvenHorizontal => even_horizontal(num_panes, parent_pane.clone()),
-            Layout::EvenVertical => even_vertical(num_panes, parent_pane.clone()),
-            Layout::MainVertical => main_vertical(num_panes, parent_pane.clone()),
-            Layout::MainVerticalFlipped => main_vertical_flipped(num_panes, parent_pane.clone()),
-            Layout::Tiled => tiled(num_panes, parent_pane.clone()),
-            Layout::ThreeColumns => three_columns(num_panes, parent_pane.clone()),
+            Layout::EvenHorizontal => even_horizontal(total_panes, parent_pane.clone()),
+            Layout::EvenVertical => even_vertical(total_panes, parent_pane.clone()),
+            Layout::MainVertical => main_vertical(total_panes, parent_pane.clone()),
+            Layout::MainVerticalFlipped => main_vertical_flipped(total_panes, parent_pane.clone()),
+            Layout::Tiled => tiled(total_panes, parent_pane.clone()),
+            Layout::ThreeColumns => three_columns(total_panes, parent_pane.clone()),
             Layout::DoubleMainVertical => None,
             Layout::DoubleMainHorizontal => None,
         }
@@ -46,14 +46,21 @@ impl Layout {
 }
 
 fn split_even(
-    num_panes: NumPanes,
+    total_panes: TotalPanes,
     parent_pane: Pane,
     direction: SplitDirection,
 ) -> Option<Vec<Pane>> {
-    let mut panes: Vec<Pane> = vec![];
+    let mut panes: Vec<Pane> = vec![parent_pane.clone()];
 
-    for p in 0..num_panes.0 - 1 {
-        let pane_perc = ((1.0 / (num_panes.0 - p) as f32) * 100.0)
+    // If there's only one pane passed, split once at 50% and return
+    if total_panes.0 == 1 {
+        let pane = parent_pane.split(Some(direction), &Some(String::from("50")), None, false);
+        panes.push(pane);
+        return Some(panes);
+    }
+
+    for p in 0..total_panes.0 - 1 {
+        let pane_perc = ((1.0 / (total_panes.0 - p) as f32) * 100.0)
             .round()
             .to_string();
         let pane = parent_pane.split(Some(direction), &Some(pane_perc), None, false);
@@ -68,14 +75,14 @@ fn split_even(
 }
 
 fn main_splits(
-    num_panes: NumPanes,
+    total_panes: TotalPanes,
     parent_pane: Pane,
     direction: SplitDirection,
 ) -> Option<Vec<Pane>> {
     let main_pane = parent_pane.split(Some(direction), &Some(String::from("50")), None, false);
 
     match split_even(
-        NumPanes(num_panes.0 - 1),
+        TotalPanes(total_panes.0 - 1),
         main_pane.clone(),
         SplitDirection::Bottom,
     ) {
@@ -88,24 +95,24 @@ fn main_splits(
     }
 }
 
-fn even_horizontal(num_panes: NumPanes, parent_pane: Pane) -> Option<Vec<Pane>> {
-    split_even(num_panes, parent_pane, SplitDirection::Right)
+fn even_horizontal(total_panes: TotalPanes, parent_pane: Pane) -> Option<Vec<Pane>> {
+    split_even(total_panes, parent_pane, SplitDirection::Right)
 }
 
-fn even_vertical(num_panes: NumPanes, parent_pane: Pane) -> Option<Vec<Pane>> {
-    split_even(num_panes, parent_pane, SplitDirection::Bottom)
+fn even_vertical(total_panes: TotalPanes, parent_pane: Pane) -> Option<Vec<Pane>> {
+    split_even(total_panes, parent_pane, SplitDirection::Bottom)
 }
 
-fn main_vertical(num_panes: NumPanes, parent_pane: Pane) -> Option<Vec<Pane>> {
-    main_splits(num_panes, parent_pane, SplitDirection::Left)
+fn main_vertical(total_panes: TotalPanes, parent_pane: Pane) -> Option<Vec<Pane>> {
+    main_splits(total_panes, parent_pane, SplitDirection::Left)
 }
 
-fn main_vertical_flipped(num_panes: NumPanes, parent_pane: Pane) -> Option<Vec<Pane>> {
-    main_splits(num_panes, parent_pane, SplitDirection::Right)
+fn main_vertical_flipped(total_panes: TotalPanes, parent_pane: Pane) -> Option<Vec<Pane>> {
+    main_splits(total_panes, parent_pane, SplitDirection::Right)
 }
 
-fn tiled(num_panes: NumPanes, parent_pane: Pane) -> Option<Vec<Pane>> {
-    let num_panes_even = num_panes.0 % 2 == 0;
+fn tiled(total_panes: TotalPanes, parent_pane: Pane) -> Option<Vec<Pane>> {
+    let total_panes_even = total_panes.0 % 2 == 0;
     let mut all_panes = vec![];
     let left_pane = parent_pane;
     let right_pane = left_pane.split(
@@ -115,17 +122,17 @@ fn tiled(num_panes: NumPanes, parent_pane: Pane) -> Option<Vec<Pane>> {
         false,
     );
 
-    if num_panes.0 == 2 {
+    if total_panes.0 == 2 {
         all_panes.push(left_pane);
         all_panes.push(right_pane);
         return Some(all_panes);
     }
 
     let per_side;
-    if num_panes_even {
-        per_side = NumPanes(num_panes.0 / 2);
+    if total_panes_even {
+        per_side = TotalPanes(total_panes.0 / 2);
     } else {
-        per_side = NumPanes((num_panes.0 - 1) / 2);
+        per_side = TotalPanes((total_panes.0 - 1) / 2);
 
         // If panes are odd, create a bottom pane at the top level
         let bottom_pane = left_pane.split(Some(SplitDirection::Bottom), &None, None, true);
@@ -141,30 +148,31 @@ fn tiled(num_panes: NumPanes, parent_pane: Pane) -> Option<Vec<Pane>> {
     Some(all_panes)
 }
 
-fn three_columns(num_panes: NumPanes, parent_pane: Pane) -> Option<Vec<Pane>> {
-    // We have one pane already, so we only need 2 more columns.
-    // TODO: Not correctly splitting. Doesn't take parent_pane size into account
-    let cols = even_horizontal(NumPanes(2), parent_pane.clone()).unwrap_or(vec![]);
+fn three_columns(total_panes: TotalPanes, parent_pane: Pane) -> Option<Vec<Pane>> {
+    let cols =
+        split_even(TotalPanes(3), parent_pane.clone(), SplitDirection::Right).unwrap_or(vec![]);
     let num_cols = cols.len();
     // Column panes already created, so remove them from the total count.
-    let total_panes_to_gen = num_panes.0 - num_cols;
-    let odd_num_panes = total_panes_to_gen % num_cols != 0;
+    let total_panes_to_gen = total_panes.0 - num_cols;
+    let odd_total_panes = total_panes_to_gen % num_cols != 0;
     let panes_per_col = (total_panes_to_gen as f32 / 3.0).ceil();
     let mut panes = vec![];
 
     for (i, p) in cols.iter().enumerate() {
-        if i == num_cols && odd_num_panes {
+        if i == num_cols - 1 && odd_total_panes {
             break;
         }
 
-        // TODO: Not correctly splitting. Can't take into account parent pane size.
-        let v_panes = even_vertical(NumPanes(panes_per_col as usize), p.clone());
+        let v_panes = split_even(
+            TotalPanes(panes_per_col as usize),
+            p.clone(),
+            SplitDirection::Bottom,
+        );
 
         if let Some(mut created_panes) = v_panes {
             panes.append(&mut created_panes);
         }
     }
 
-    println!("Panes: {:?}", panes);
     Some(panes)
 }
