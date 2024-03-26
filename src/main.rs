05f08f001dc4e8ed9686ca43d1278f21179ce087
Alex Caza <alex@alexcaza.com>
2024-03-26 17:34:42 -0400

    Allow users to specify layout with .yml
    Also better handles errors for config path functions

--- src/main.rs
+++ src/main.rs
@@ -82,11 +82,10 @@ fn layout_string_to_enum(name: &str) -> Layout {
     }
 }
 
-fn get_global_config_path() -> String {
+fn get_global_config_path() -> Result<String, String> {
     let mut path = String::from("");
     for dir in DIRS {
         let _dir = format!("~/{}", dir);
-        println!("dir: {}", _dir);
         let expanded = tilde(&_dir);
         let paths = fs::read_dir(expanded.deref());
         match paths {
@@ -98,14 +97,13 @@ fn get_global_config_path() -> String {
     }
 
     if path == "" {
-        println!("Couldn't find a .weztermocil, .teamocil or .itermocil folder in the home directory (~)\nPlease make sure one of them exists before continuing");
-        process::exit(1);
+        return Err(String::from("Couldn't find a .weztermocil, .teamocil or .itermocil folder in the home directory (~)\nPlease make sure one of them exists before continuing"));
     }
 
-    path
+    Ok(path)
 }
 
-fn get_local_config_path() -> String {
+fn get_local_config_path() -> Result<String, String> {
     let mut path = String::from("");
     for dir in DIRS {
         let paths = fs::read_dir(dir);
@@ -118,23 +116,30 @@ fn get_local_config_path() -> String {
     }
 
     if path == "" {
-        println!("Couldn't find a .weztermocil, .teamocil or .itermocil folder in the current directory\nPlease make sure one of them exists before continuing");
-        process::exit(1);
+        return Err(String::from("Couldn't find a .weztermocil, .teamocil or .itermocil folder in the current directory\nPlease make sure one of them exists before continuing"));
     }
 
-    path
+    Ok(path)
 }
 
 fn list_layouts() {
     let path = get_global_config_path();
-    // We've already validated that the path exists, so we can unwrap here.
-    let entries = fs::read_dir(path).unwrap();
-    for entry in entries {
-        println!("{}", entry.unwrap().file_name().into_string().unwrap());
+    match path {
+        Ok(p) => {
+            // We've already validated that the path exists, so we can unwrap here.
+            let entries = fs::read_dir(p).unwrap();
+            for entry in entries {
+                println!("{}", entry.unwrap().file_name().into_string().unwrap());
+            }
+        }
+        Err(e) => {
+            println!("{}", e);
+            process::exit(1);
+        }
     }
 }
 
-fn get_path_for_layout_file(layout_name: String) -> Result<String, String> {
+fn get_path_for_layout_file(layout_name: &str) -> Result<String, String> {
     let current_dir = std::env::current_dir()
         .unwrap()
         .into_os_string()
@@ -148,19 +153,29 @@ fn get_path_for_layout_file(layout_name: String) -> Result<String, String> {
     }
 
     let local_layout_dir_path = get_local_config_path();
-    let local_layout_dir_fp = format!("{}/{}/{}", current_dir, local_layout_dir_path, layout_name);
-    let in_local_layout_dir = fs::File::open(&local_layout_dir_fp);
+    match local_layout_dir_path {
+        Ok(p) => {
+            let local_layout_dir_fp = format!("{}/{}/{}", current_dir, p, layout_name);
+            let in_local_layout_dir = fs::File::open(&local_layout_dir_fp);
 
-    if let Ok(_) = in_local_layout_dir {
-        return Ok(local_layout_dir_fp);
+            if let Ok(_) = in_local_layout_dir {
+                return Ok(local_layout_dir_fp);
+            }
+        }
+        Err(_) => {}
     }
 
     let global_path = get_global_config_path();
-    let global_layout_fp = format!("{}/{}", global_path, layout_name);
-    let in_global_layout = fs::File::open(&global_layout_fp);
+    match global_path {
+        Ok(p) => {
+            let global_layout_fp = format!("{}/{}", p, layout_name);
+            let in_global_layout = fs::File::open(&global_layout_fp);
 
-    if let Ok(_) = in_global_layout {
-        return Ok(global_layout_fp);
+            if let Ok(_) = in_global_layout {
+                return Ok(global_layout_fp);
+            }
+        }
+        Err(_) => {}
     }
 
     return Err(String::from("Couldn't find layout"));
@@ -198,7 +213,7 @@ fn main() {
     }
 
     if let Some(path) = args.show {
-        let path = get_path_for_layout_file(path);
+        let path = get_path_for_layout_file(&path);
         match path {
             Ok(p) => {
                 println!("Found layout at path: {}", p);
@@ -213,7 +228,8 @@ fn main() {
     }
 
     if let Some(path) = args.edit {
-        let path = get_path_for_layout_file(path);
+        let layout = qualify_layout_file(&path);
+        let path = get_path_for_layout_file(&layout);
         match path {
             Ok(p) => {
                 println!("Found layout at path: {}", p);
@@ -231,7 +247,8 @@ fn main() {
     let mut layout_path: String = String::from("");
 
     if let Some(global_layout) = args.global_layout {
-        let path = get_path_for_layout_file(global_layout);
+        let layout = qualify_layout_file(&global_layout);
+        let path = get_path_for_layout_file(&layout);
         match path {
             Ok(p) => {
                 layout_path = p;
@@ -243,8 +260,9 @@ fn main() {
         }
     }
 
-    if let Some(layout) = args.layout {
-        let f = match fs::canonicalize(layout.as_str()) {
+    if let Some(path) = args.layout {
+        let layout = qualify_layout_file(&path);
+        let f = match fs::canonicalize(&layout) {
             Ok(path) => path.into_os_string().into_string(),
             Err(_) => {
                 println!("Couldn't find file at path: {}. Does it exist?", layout);
@@ -281,6 +299,14 @@ fn main() {
     match focus_pane.focus() {
         Ok(res) => res,
         Err(error) => println!("{:?}", error),
+    }
+}
+
+fn qualify_layout_file(path: &str) -> String {
+    if path.contains(".yml") == false {
+        format!("{}.yml", path)
+    } else {
+        String::from(path)
     }
 }
 
